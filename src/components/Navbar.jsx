ddeeecb5e6c211fadb9c538059bc4c4e097ac397
priyanshu-ch29 <priyanshuchaudhary1000@gmail.com>
2024-05-29 12:27:13 +0530

    changes

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -147,12 +147,15 @@ function Navabar() {
             </span>
             Swiggy Corporate
           </li>
-          <li className="flex hover:cursor-pointer font-bold hover:text-orange-600 items-center">
-            <span className="m-1">
-              <IoSearch />
-            </span>
-            Search
-          </li>
+          <Link to="/searchRestaurant">
+            <li className="flex hover:cursor-pointer font-bold hover:text-orange-600 items-center">
+              <span className="m-1">
+                <IoSearch />
+              </span>
+              Search
+            </li>
+          </Link>
+
           <li className="flex hover:cursor-pointer font-bold hover:text-orange-600 items-center">
             <span className="m-1">
               <LuBadgePercent />
