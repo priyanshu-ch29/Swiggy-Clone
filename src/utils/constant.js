bd64bb9dea35a4ef76ceb39a322548c53f36462c
priyanshu-ch29 <priyanshuchaudhary1000@gmail.com>
2024-06-29 02:16:05 +0530

    changes

--- src/utils/constant.js
+++ src/utils/constant.js
@@ -2,7 +2,7 @@ export const CDN_URL =
   "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/";
 
 export const MENU_API =
-  "https://proxy.cors.sh/https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=28.6691565&lng=77.45375779999999&restaurantId=";
+  "https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=28.6691565&lng=77.45375779999999&restaurantId=";
 
 export const ITEM_URL =
   "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_300,h_300,c_fit/";
