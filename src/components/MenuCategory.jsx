606f80b26d675e9cd2fa18a3d0b3d7511fe817df
priyanshu-ch29 <priyanshuchaudhary1000@gmail.com>
2024-07-21 17:09:39 +0530

    add responsiveness

--- src/components/MenuCategory.jsx
+++ src/components/MenuCategory.jsx
@@ -7,8 +7,8 @@ const MenuCategory = (data) => {
   // taking the menu item category  data as prop
   const [arrow, setArrow] = useState(false);
   return (
-    <div className="m-5 p-3 w-[55rem] mx-auto border-t-[20px] max-sm:w-[400px] max-md:w-[700px] ">
-      <div className="flex justify-between max-md:w-[680px] max-sm:w-[400px] max-lg:[900px]">
+    <div className="m-5 p-3 w-[55rem] mx-auto border-t-[20px] max-sm:w-[350px] max-md:w-[700px] ">
+      <div className="flex justify-between max-md:w-[680px] max-sm:w-[350px] max-lg:[900px]">
         <h1 className="font-bold text-[18px]">
           {data.data.title}({data.data.itemCards.length})
         </h1>
